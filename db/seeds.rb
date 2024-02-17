bae582dcf68659841c6b45783ba01bbcb002660a
João Victor Chaves <joao.victor.chaves159@gmail.com>
2024-02-17 15:06:46 -0300

    Feat(SEED):inicio implementacao da seed para preencher modalidades inicias do sistema

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+["moto", "bicicleta"].each do |gerar_modalidade|
+  Modalidade.find_or_create_by!(nome: gerar_modalidade)
+end
